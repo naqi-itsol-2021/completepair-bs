99c52e3fb4603402e938b46df50bfcfd5b82adb8
Syed Naqi <Syed.naqi@itsolution24x7.net>
2022-03-07 05:03:44 +0500

    Branding Changed

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,18 +1,20 @@
 export default function Logo({ small, icon }: { small?: boolean; icon?: boolean }) {
   return (
-    <h1 className="inline">
-      <strong>
+    <h1 className="inline logo-cp">
         {icon ? (
-          <img className="w-9 mx-auto" alt="Cal" title="Cal" src="/cal-com-icon-white.svg" />
+          <img className="w-9 mx-auto" alt="Cal" title="Cal" src="/cp-logo.jpg" />
         ) : (
           <img
-            className={small ? "h-4 w-auto" : "h-5 w-auto"}
-            alt="Cal"
-            title="Cal"
-            src="/calendso-logo-white-word.svg"
+          className={small ? "h-4 w-auto" : "h-5 w-auto"}
+          alt="Cal"
+          title="Cal"
+          width="100"
+          src="/cp-logo.jpg"
           />
         )}
-      </strong>
+        <p>
+          CompletePair
+        </p>
     </h1>
   );
 }
