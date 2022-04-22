1d7f2a028e443291e2d286b427fe56b774896399
FarazAhmed786 <97106160+FarazAhmed786@users.noreply.github.com>
2022-04-22 17:44:47 +0500

    Get all Bookings

---
+++ pages/api/booking.ts
@@ -0,0 +1,28 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+import { getSession } from "@lib/auth";
+import prisma from "@lib/prisma";
+import slugify from "@lib/slugify";
+import { trpc } from "@lib/trpc";
+
+import sessionHandler from "../middlewares/sessionHandler";
+
+export async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const session = await getSession({ req: req });
+  if (!session?.user?.id) {
+    res.status(401).json({ message: "Not authenticated" });
+    return;
+  }
+
+  // Created to fetch all teams by User ID
+  if (req.method == "GET") {
+    const Booking = await prisma.booking.findMany({
+      include: { user: true },
+    });
+
+    return res.status(200).json({ Bookings: Booking });
+  }
+
+  res.status(404).json({ message: "Booking not found" });
+}
+export default sessionHandler(handler);
