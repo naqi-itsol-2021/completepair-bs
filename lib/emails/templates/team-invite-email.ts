ec24d5b3cd07fe8e80ffeccd60e70afce3371587
FarazAhmed786 <97106160+FarazAhmed786@users.noreply.github.com>
2022-09-01 16:13:19 +0500

    add

--- lib/emails/templates/team-invite-email.ts
+++ lib/emails/templates/team-invite-email.ts
@@ -78,7 +78,7 @@ export default class TeamInviteEmail {
     ${emailHead(headerContent)}
     <body style="word-spacing:normal;background-color:#F5F5F5;">
       <div style="background-color:#F5F5F5;">
-       
+      ${emailBodyLogo()}
         <!--[if mso | IE]></td></tr></table><table align="center" border="0" cellpadding="0" cellspacing="0" class="" style="width:600px;" width="600" ><tr><td style="line-height:0px;font-size:0px;mso-line-height-rule:exactly;"><![endif]-->
         <div style="margin:0px auto;max-width:600px;">
           <table align="center" border="0" cellpadding="0" cellspacing="0" role="presentation" style="width:100%;">
